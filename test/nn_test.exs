3eb2f4b71ff2c8d37971043df7ca6a6838d7fa50
kwisatzheadache <michaelhorvath87@gmail.com>
2017-04-18 19:09:51 +0200

    fix mix test

--- test/nn_test.exs
+++ test/nn_test.exs
@@ -5,6 +5,8 @@ defmodule NnTest do
 
   test "exoself" do
     Genotype.construct("henry.txt", "rng", "pts", [3,3,3])
+    IO.inspect Genotype.read("henry.txt")
     Exoself.map("henry.txt")
+    File.rm!("henry.txt")
   end
 end
