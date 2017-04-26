a3040b2538f2c3e66182fbd76f0acbe71e4698a2
kwisatzheadache <michaelhorvath87@gmail.com>
2017-04-26 14:12:46 +0200

    Sensor.create runs, but Macro seems to be doing nothing.

--- lib/macrotest.ex
+++ lib/macrotest.ex
@@ -3,6 +3,25 @@ defmodule MacroTest do
     5
   end
 
+  defmacro type(sensor_name) do
+    IO.inspect sensor_name, label: "sensor_name"
+    {name, location, _} = sensor_name
+    IO.inspect name, label: "name"
+    case is_atom(name) do
+      true -> IO.puts "Sensor.type loaded"
+              #> quote do Sensor.create end
+              #> {{:., [], [{:__aliases__, [alias: false], [:Sensor]}, :create]}, [], []}
+              #  So, we should be able to substitue sensor_name for :create. Should require that sensor_name
+              #  be an atom, while we're at it.
+              #> quote do IO.puts "hello"
+              #> {{:., [], [{:__aliases__, [alias: false], [:IO]}, :puts]}, [], ["hello"]}
+              #  Perhaps I'm looking for Code.eval_quoted(ast_expression)
+              ast = {{:., [], [{:__aliases__, [alias: false], [:Morphology]}, name]}, [], [:sensor]}
+              IO.puts Macro.to_string(ast)
+              Code.eval_quoted(ast)
+      false -> "sensor must be an atom"
+    end
+  end
   # def print(variable) do
   #   IO.puts variable
   #   IO.puts test
